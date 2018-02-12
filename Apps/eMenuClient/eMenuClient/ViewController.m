060dc264a8d230576f8701a83623ba742c748865
Realank <realank@126.com>
2018-02-12 15:18:42 +0800

    revert project

---
+++ Apps/eMenuClient/eMenuClient/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  eMenuClient
+//
+//  Created by Realank on 2018/2/12.
+//  Copyright © 2018年 Realank. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
